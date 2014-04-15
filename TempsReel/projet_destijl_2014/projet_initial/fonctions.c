be8bc72e49c2655596073036978450d16d1d7b87
Marie-Charlotte Bideaud <mcbideaud@gmail.com>
2014-04-15 10:06:58 +0200

    Ajout de la fonction batterie (ne fonctionne pas)

--- TempsReel/projet_destijl_2014/projet_initial/fonctions.c
+++ TempsReel/projet_destijl_2014/projet_initial/fonctions.c
@@ -35,10 +35,11 @@ void connecter(void * arg) {
         rt_mutex_release(&mutexEtat);
 
         if (status == STATUS_OK) {
-            status = robot->start_insecurely(robot);
+            status = robot->start_insecurely(robot);    // Attente des ordres
             if (status == STATUS_OK){
-                rt_printf("tconnect : Robot démarrer\n");
+                rt_printf("tconnect : Robot demarre\n");
             }
+            // gerer les autres cas (cas d'erreurs)
         }
 
         message = d_new_message();
@@ -55,7 +56,7 @@ void connecter(void * arg) {
 
 void communiquer(void *arg) {
     DMessage *msg = d_new_message();
-    int var1 = 1;
+    int var1 = 1;   // Var qui represente le nombre d'octets recus
     int num_msg = 0;
 
     rt_printf("tserver : Début de l'exécution de serveur\n");
@@ -71,33 +72,62 @@ void communiquer(void *arg) {
         var1 = serveur->receive(serveur, msg);
         num_msg++;
         if (var1 > 0) {
-            switch (msg->get_type(msg)) {
+            switch (msg->get_type(msg)) {   /* Todo : ajouter les autres types de messages (MESSAGE_TYPE_CHAR, 							MESSAGE_TYPE_IMAGE, MESSAGE_TYPE_INT, MESSAGE_TYPE_MISSION, MESSAGE_TYPE_ORDER, 						MESSAGE_TYPE_POSITION, MESSAGE_TYPE_STATE, MESSAGE_TYPE_STRING, MESSAGE_TYPE_UNKNOWN, 							MESSAGE_TYPE_VERSION) */
                 case MESSAGE_TYPE_ACTION:
-                    rt_printf("tserver : Le message %d reçu est une action\n",
-                            num_msg);
+                    rt_printf("tserver : Le message %d reçu est une action\n", num_msg);
                     DAction *action = d_new_action();
                     action->from_message(action, msg);
-                    switch (action->get_order(action)) {
-                        case ACTION_CONNECT_ROBOT:
+                    switch (action->get_order(action)) {    
+                        case ACTION_CONNECT_ROBOT:          /* Todo : ajouter les autres types d'actions (#ACTION_FIND_ARENA, 									#ACTION_ARENA_FAILED, #ACTION_ARENA_IS_FOUND, 									#ACTION_COMPUTE_CONTINUOUSLY_POSITION, #ACTION_ARENA_FAILED)*/
                             rt_printf("tserver : Action connecter robot\n");
                             rt_sem_v(&semConnecterRobot);
                             break;
                     }
                     break;
-                case MESSAGE_TYPE_MOVEMENT:
-                    rt_printf("tserver : Le message reçu %d est un mouvement\n",
-                            num_msg);
+                case MESSAGE_TYPE_MOVEMENT:                 
+                    rt_printf("tserver : Le message reçu %d est un mouvement\n", num_msg);
                     rt_mutex_acquire(&mutexMove, TM_INFINITE);
                     move->from_message(move, msg);
                     move->print(move);
                     rt_mutex_release(&mutexMove);
+                    break;
+                    
+                default:
+                    rt_printf("ERREUR tserver : Le message recu %d a un type indefini\n", num_msg);
                     break;
             }
         }
     }
 }
 
-void deplacer(void *arg) {
+void etat_batterie(void *arg) {
+    int batterie = -1 ;    
+    DMessage *message;
+    
+    rt_printf("tbatterie : Debut de l'éxecution de periodique à 250ms\n");
+    rt_task_set_periodic(NULL, TM_NOW, 250000000);
+    
+     while (1) {
+        /* Attente de l'activation périodique */
+        rt_task_wait_period(NULL);
+        rt_printf("tbatterie : Activation périodique\n");
+        
+    	robot->get_vbat(robot, &batterie);   // Renvoie 0, 1 ou 2 (= etat de la batterie)
+    	// rt_printf("etat de la batterie : %d\n", batterie); 
+    
+   	message = d_new_message();
+   	message->put_state(message, batterie);
+
+   	rt_printf("tbatterie : Envoi message\n");
+   	message->print(message, 100);
+
+   	if (write_in_queue(&queueMsgGUI, message, sizeof (DMessage)) < 0) {
+            message->free(message);
+   	}
+     }
+}    
+
+void deplacer(void *arg) {   // Argument = mouvement ?
     int status = 1;
     int gauche;
     int droite;
@@ -109,7 +139,7 @@ void deplacer(void *arg) {
     while (1) {
         /* Attente de l'activation périodique */
         rt_task_wait_period(NULL);
-        rt_printf("tmove : Activation périodique ---STATUS: %d---\n", status);
+        rt_printf("tmove : Activation périodique\n");
 
         rt_mutex_acquire(&mutexEtat, TM_INFINITE);
         status = etatCommRobot;
@@ -157,10 +187,6 @@ void deplacer(void *arg) {
                 }
             }
         }
-        else
-   		{
-   			printf("\n\n****** ERREUR - status: %d ******\n\n", status);
-   		}
     }
 }
 
