29926fff55c37ae506e3b0d2215938b82fb4649f
Marjorie Dugrand <marjorie.dugrand@gmail.com>
2014-05-17 10:53:49 +0200

    création mission à part

---
+++ TempsReel/projet_destijl_2014/projet_initial/mission.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   fonctions.h
+ * Author: mdugrand
+ *
+ * Created on 17 mai 2014 10:49
+ */
+
+#ifndef MISSION_H
+#define	MISSION_H
+
+#include "global.h"
+#include "includes.h"
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+        void mission_reach_coordinates(void *arg);
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MISSION_H */
+
